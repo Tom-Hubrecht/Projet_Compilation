c308f6476d824b1117cf13c7e7a08ee3ca39b2f2
Tom-Hubrecht <tom@hubrecht.ovh>
2019-12-12 16:56:30 +0100

    Update the assembly process

--- src/go_assembler.ml
+++ src/go_assembler.ml
@@ -5,12 +5,53 @@ open Format
 open X86_64
 open Go_ast
 
+module Smap = Map.Make(String)
+
+let lab_count = ref 0
+let str_count = ref 0
+
+let str_list = ref []
+
+let new_lab () =
+  incr lab_count;
+  "__lab_"^(string_of_int !lab_count)
+
+let new_str s =
+  incr str_count;
+  let lab_s = ".Str_"^(string_of_int !str_count) in
+  str_list := (lab_s, s)::!str_list;
+  lab lab_s
+
 let print_type = function
   | Tint -> "print_int"
   | Tbool -> "print_bool"
   | Tstring -> "print_string"
-  | Tstruct s -> "print_"^s
+  | Tstruct s -> "print_struct_"^s
   | Pointer _ -> "print_pointer"
+  | _ -> assert false
+
+let set_sizes s_env =
+  let n_env = ref Smap.empty in
+  let rec aux s =
+    try fst (Smap.find s !n_env) with Not_found ->
+      let fields = Smap.find s s_env in
+      let f', size = Smap.fold
+                   (fun x t (f, i) ->
+                      match t with
+                       | Tint | Tbool | Tstring | Pointer _ ->
+                         (Smap.add x (t, i) f, i + 1)
+                       | Tstruct s' ->
+                         (Smap.add x (t, i) f, i + aux s')
+                       | _ -> assert false) fields (Smap.empty, 0) in
+      n_env := Smap.add s (size, f') !n_env;
+      size
+  in
+  Smap.iter (fun s _ -> let _ = aux s in ()) s_env;
+  !n_env
+
+let get_size s_env = function
+  | Tint | Tbool | Tstring | Pointer _ -> 1
+  | Tstruct s -> fst (Smap.find s s_env)
   | _ -> assert false
 
 (* Instructions d'affichage de base *)
@@ -33,6 +74,11 @@ let fmt_print_base =
   call "printf" ++
   ret ++
 
+  label "print_string" ++
+  movq (imm 0) !%rax ++
+  call "printf" ++
+  ret ++
+
   label "print_newline" ++
   leaq (lab ".Sprint_newline") rdi ++
   movq (imm 0) !%rax ++
@@ -43,13 +89,29 @@ let fmt_print_base =
   leaq (lab ".Sprint_space") rdi ++
   movq (imm 0) !%rax ++
   call "printf" ++
+  ret ++
+
+  label "print_lbra" ++
+  leaq (lab ".Sprint_space") rdi ++
+  movq (imm 0) !%rax ++
+  call "printf" ++
+  ret ++
+
+  label "print_rbra" ++
+  leaq (lab ".Sprint_rbra") rdi ++
+  movq (imm 0) !%rax ++
+  call "printf" ++
   ret
 
-let fmt_print s_env =
-  (*let print_struct s =
-    label ("print_"^s) ++
+let fmt_print s_env v_env =
+  (* TODO : print_strucures
+  let rec print_fields
+  let print_struct s =
+    label ("print_struct_"^s) ++
+    call "print_lbra" ++
     nop
-  in*)
+  in
+  *)
   fmt_print_base
 
 let print_labels =
@@ -59,25 +121,9 @@ let print_labels =
   label ".Sprint_nil" ++ string "<nil>" ++
   label ".Sprint_pointer" ++ string "Ox%d" ++
   label ".Sprint_newline" ++ string "\n" ++
-  label ".Sprint_space" ++ string " "
-
-(* Instructions pour les opérations binaires avec e1 dans %rdi et e2 dans %rsi
- * et place le résultat dans %rdi *)
-let compile_binop = function
-  | Badd -> addq !%rsi !%rdi
-  | Bsub -> subq !%rsi !%rdi
-  | Bmul -> imulq !%rsi !%rdi
-  | Bdiv ->
-    movq !%rdi !%rax ++
-    movq (imm 0) !%rdx ++
-    idivq !%rsi ++
-    movq !%rax !%rdi
-  | Bmod ->
-    movq !%rdi !%rax ++
-    movq (imm 0) !%rdx ++
-    divq !%rsi ++
-    movq !%rdx !%rdi
-  | _ -> assert false
+  label ".Sprint_space" ++ string " " ++
+  label ".Sprint_lbra" ++ string "{" ++
+  label ".Sprint_rbra" ++ string "}"
 
 (* Stockage des constantes *)
 let compile_cst = function
@@ -85,17 +131,106 @@ let compile_cst = function
   | Cbool false -> movq (imm 0) !%rdi
   | Cbool true -> movq (imm 1) !%rdi
   | Cnil -> movq (imm 0) !%rdi
-  | _ -> assert false
+  | Cstring s -> leaq (new_str s) rdi
 
 (* La valeur calculéee se trouve dans le registre %rdi *)
 let rec compile_expr = function
   | _, Tecst c ->
     compile_cst c
+  | _, Teunop (u, e) ->
+    compile_unop e u
   | _, Tebinop (b, e1, e2) ->
+    compile_binop e1 e2 b
+  | _ -> assert false
+
+(* Instructions unaires *)
+and compile_unop e = function
+  | Unot ->
+    compile_expr e ++
+    cmpq !%rdi (imm 0) ++
+    sete !%rdi
+  | Uneg ->
+    compile_expr e ++
+    negq !%rdi
+  | Uref
+  | Uderef -> (* TODO *) assert false
+
+(* Instructions pour les opérations binaires avec e1 dans %rdi et e2 dans %rsi
+ * et place le résultat dans %rdi *)
+and compile_binop e1 e2 = function
+  | Badd ->
     compile_expr e2 ++
     movq !%rdi !%rsi ++
     compile_expr e1 ++
-    compile_binop b
+    addq !%rsi !%rdi
+  | Bsub ->
+    compile_expr e2 ++
+    movq !%rdi !%rsi ++
+    compile_expr e1 ++
+    subq !%rsi !%rdi
+  | Bmul ->
+    compile_expr e2 ++
+    movq !%rdi !%rsi ++
+    compile_expr e1 ++
+    imulq !%rsi !%rdi
+  | Bdiv ->
+    compile_expr e1 ++
+    movq !%rdi !%rax ++
+    cqto ++
+    compile_expr e2 ++
+    idivq !%rdi ++
+    movq !%rax !%rdi
+  | Bmod ->
+    compile_expr e1 ++
+    movq !%rdi !%rax ++
+    cqto ++
+    compile_expr e2 ++
+    idivq !%rdi ++
+    movq !%rdx !%rdi
+  | Band ->
+    let lab = new_lab () in
+    compile_expr e1 ++
+    testq !%rdi !%rdi ++
+    je lab ++
+    (* If we didn't jump then only the value of e2 matters *)
+    compile_expr e2 ++
+    label lab
+  | Bor ->
+    let lab = new_lab () in
+    compile_expr e1 ++
+    testq !%rdi !%rdi ++
+    jne lab ++
+    (* If we didn't jump then only the value of e2 matters *)
+    compile_expr e2 ++
+    label lab
+  | Blt ->
+    compile_expr e1 ++
+    movq !%rdi !%rsi ++
+    compile_expr e2 ++
+    cmpq !%rdi !%rsi ++
+    setl !%dil ++
+    andq (imm 1) !%rdi
+  | Ble ->
+    compile_expr e1 ++
+    movq !%rdi !%rsi ++
+    compile_expr e2 ++
+    cmpq !%rdi !%rsi ++
+    setle !%dil ++
+    andq (imm 1) !%rdi
+  | Bgt ->
+    compile_expr e1 ++
+    movq !%rdi !%rsi ++
+    compile_expr e2 ++
+    cmpq !%rdi !%rsi ++
+    setg !%dil ++
+    andq (imm 1) !%rdi
+  | Bge ->
+    compile_expr e1 ++
+    movq !%rdi !%rsi ++
+    compile_expr e2 ++
+    cmpq !%rdi !%rsi ++
+    setge !%dil ++
+    andq (imm 1) !%rdi
   | _ -> assert false
 
 (* Compilation d'un appel à print *)
@@ -103,43 +238,70 @@ let rec compile_print = function
   | [] ->
     call "print_newline"
   | (t, _ as e)::q ->
-    (compile_expr e) ++
+    compile_expr e ++
     call (print_type t) ++
     call "print_space" ++
     compile_print q
 
 (* On compile les instructions *)
 let rec compile_instr = function
-  | Tiempty -> nop
+  | Tiempty ->
+    nop
   | Tibloc l ->
     let b_code = List.map compile_instr l in
     List.fold_right (++) b_code nop
-  | Tiexpr e -> compile_expr e
-  | Tiprint l -> compile_print l
+  | Tiif (e, i1, i2) ->
+    let lab_false = new_lab () and lab_true = new_lab () in
+    compile_expr e ++
+    testq !%rdi !%rdi ++
+    je lab_false ++
+    compile_instr i1 ++
+    jmp lab_true ++
+    label lab_false ++
+    compile_instr i2 ++
+    label lab_true
+  | Tiprint l ->
+    compile_print l
+  | Tiexpr e ->
+    compile_expr e
+  | Tiexec _ -> (* TODO *) assert false
+  | Tiincr _ -> (* TODO *) assert false
+  | Tidecr _ -> (* TODO *) assert false
+  | Tifor (e, i) ->
+    let lab_end = new_lab () and lab_start = new_lab () in
+    label lab_start ++
+    compile_expr e ++
+    testq !%rdi !%rdi ++
+    je lab_end ++
+    compile_instr i ++
+    jmp lab_start ++
+    label lab_end
   | _ -> assert false
 
 (* On ne compile pas les structures, uniquement les fonctions *)
 let compile_decl = function
   | Tdfunc ((_, _, f), v, t, b) ->
     label ("func_"^f) ++
-    compile_instr b
+    compile_instr b ++
+    ret
   | Tdstruct _ -> nop
 
 (* Compile le programme p et enregistre le code dans le fichier ofile *)
-let compile_program (b, p, s_env, f_env) ofile =
+let compile_program (b, p, s_env, v_env, f_env) ofile =
+  let s_env = set_sizes s_env in
   let code = List.map compile_decl p in
   let code = List.fold_right (++) code nop in
   let p =
     { text =
-        globl "main" ++ label "main" ++
-        nop (* À COMPLÉTER *) ++
+        globl "main" ++ label "main" ++ jmp "func_main" ++
         code ++
         ret ++
         (* On ne rajoute les instructions d'affichage que si fmt est importé *)
-        (if b then fmt_print s_env else nop);
+        (if b then fmt_print s_env v_env else nop);
       data =
         (*Hashtbl.fold (fun x _ l -> label x ++ dquad [1] ++ l) genv*)
-          (print_labels)
+        List.fold_left (fun c (l, s) -> label l ++ string s ++ c )
+          print_labels !str_list
     }
   in
   let f = open_out ofile in
